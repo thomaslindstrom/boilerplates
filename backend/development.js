8b53a56f2410e9a78f52634e93f6d4f47aa7bae0
Thomas Lindstrøm <t@hom.as>
2019-01-30 14:36:11 +0100

    Handle folder handlers

--- backend/development.js
+++ backend/development.js
@@ -2,8 +2,7 @@ import http from 'http';
 
 const HANDLERS_FOLDER = './handlers';
 const server = http.createServer(async (request, response) => {
-	const path = request.url;
-	let handlerPath;
+	const path = parseUrl(request.url, true).pathname;
 
 	if (path === '/') {
 		handlerPath = `${HANDLERS_FOLDER}/index.js`;
@@ -12,9 +11,17 @@ const server = http.createServer(async (request, response) => {
 	}
 
 	try {
-		const handler = await import(handlerPath);
+		let handler;
+
+		try {
+			handler = await import(`${HANDLERS_FOLDER + path}/index.js`);
+		} catch (error) {
+			handler = await import(`${HANDLERS_FOLDER + path}.js`);
+		}
+
 		await handler.default(request, response);
 	} catch (error) {
+		// console.error(error);
 		response.end('404 Not Found');
 	}
 });
